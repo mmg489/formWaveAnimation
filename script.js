67e37d35e3e061c8084c100eca3692cbe3c7defe
Magi Munoz <m_mg489@outlook.com>
2023-06-18 23:17:19 -0400

    styling for the form and input fields

---
+++ script.js
@@ -0,0 +1,4 @@
+/*Using JS, we are going to wrap each letter in a span, a transition is going to be applied
+to move the transition up using transform:translateY, and we are going to add a transition delay
+so that it creates the wave effect*/ 
+
