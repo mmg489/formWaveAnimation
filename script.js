35b004a81147e8eb4316732670f0688f2cbeeecb
Magi Munoz <m_mg489@outlook.com>
2023-06-19 18:52:18 -0400

    adding logic for span manipulation using .map and .join among others

--- script.js
+++ script.js
@@ -2,3 +2,19 @@
 to move the transition up using transform:translateY, and we are going to add a transition delay
 so that it creates the wave effect*/ 
 
+
+const labels =document.querySelectorAll('.form-control label')
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+    /*this ^^^ is taking all the letters in the label (so where you see email and password)
+    and grabbing the inner text then in the next line we call the split
+    which splits the letters into an array */
+        .split('')
+        .map((letter,idx) => `<span style="transition-delay:${idx*50}ms">${letter}</span>`)
+        /*.map is basically manipulating the split letters that are now an array into 
+        having each individual letter be wrapped by its own span, that way we can apply the 
+        transition that will create the wave effect to each letter individually. 
+        After that we are using .join to convert the letters back into a string.*/
+        .join('')
+})
